c667bc7d84a17a128d4d609d953540727efa8d72
Cinzia Pulvirenti <cinzia2092@hotmail.it>
2023-04-11 17:35:59 +0200

    Feat stampate card movies

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,13 +4,17 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class PageController extends Controller
 {
     public function homepage()
     {
         $movies = Movie::all();
+        $data = [
+            'movies' => $movies
+        ];
      
-        return view('welcome', $movies);
+        return view('welcome', $data);
     }
 }
